1767edb6efdfd75f13ada3eabe47242ea1f0d090
welcomenews <62940335+welcomenews@users.noreply.github.com>
2022-02-05 19:04:54 +0300

    Create service-accounts.tf

---
+++ KUB-04/service-accounts.tf
@@ -0,0 +1,42 @@
+# файл для настройки сервисных аккаунтов. Мы создаем два аккаунта — один нужен
+# для управления группами узлов — для их автоматического масштабирования,
+# а второй — для авторизации в container registry — чтобы мы могли скачивать и
+# заливать наши образы в yandex container registry.
+
+resource "yandex_iam_service_account" "docker" {
+  name        = "docker"
+  description = "service account to use container registry"
+}
+
+resource "yandex_iam_service_account" "instances" {
+  name        = "instances"
+  description = "service account to manage VMs"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "editor" {
+  folder_id = var.yc_folder_id
+
+  role = "editor"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.instances.id}",
+  ]
+
+  depends_on = [
+    yandex_iam_service_account.instances
+  ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "pusher" {
+  folder_id = var.yc_folder_id
+
+  role = "container-registry.images.pusher"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.docker.id}",
+  ]
+
+  depends_on = [
+    yandex_iam_service_account.docker
+  ]
+}
